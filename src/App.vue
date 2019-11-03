4023ea7fdfe9d74bb8783610d4a10ea435c6fe24
Yavor Spassov <ygspasov@gmail.com>
2019-11-04 00:08:48 +0200

    Adding Axios and Vue Material, and sending data to the SingleEpisode component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <Home></Home>
   </div>
 </template>
 
+<script>
+import Home from "./components/Home";
+export default {
+  components: {
+    Home
+  }
+};
+</script>
+
 <style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Roboto", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/SingleEpisode.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>{{episode.name}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    episode: { type: Object, required: true }
+  }
+};
+</script>
+
+<style>
+</style>
